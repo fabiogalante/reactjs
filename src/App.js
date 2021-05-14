829c10812ba44223a86656ef92ee783f5b370071
Fabio Galante Mans <fabiogalantemans@gmail.com>
2021-05-14 08:15:21 -0300

    Componentes de classe com estado (stateful)

--- src/App.js
+++ src/App.js
@@ -1,69 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 import {Component} from "react";
 
 
 class App extends Component {
-  state = {
-    name: 'Fabio Galante',
-    counter: 0
-  };
+    state = {
+        posts: [
+            {
+                id: 1,
+                title: 'O título 1',
+                body: 'O corpo 1'
+            }, {
+                id: 2,
+                title: 'O título 1',
+                body: 'O corpo 1'
+            }, {
+                id: 3,
+                title: 'O título 3',
+                body: 'O corpo 3'
+            },
+        ]
+    };
 
-  handleClick = () => { //sempre que precisar do this usar arrow function
-    this.setState({name: 'Junior'})
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    const {counter} = this.state;
-    this.setState({counter: counter + 1})
-  }
-
-
-  render() {
-    const {name, counter} = this.state; // ou const nome = this.state.name;
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p onClick={this.handleClick}
-            >
-              {name} {counter}
-            </p>
-            <a onClick={this.handleAClick}
-               className="App-link"
-               href="https://reactjs.org"
-               target="_blank"
-               rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-    )
-  }
+    render() {
+        const {posts} = this.state;
+        return (
+            <div className="App">
+                {posts.map(post => (
+                    <div key={post.id}>
+                        <h1>{post.title}</h1>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+            </div>
+        )
+    }
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//     <header className="App-header">
-//       <img src={logo} className="App-logo" alt="logo" />
-//       <div>Olá mundo</div>
-//       <p>
-//         Edit <code>src/App.js</code> and save to reload.
-//       </p>
-//       <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//       >
-//         Learn React
-//       </a>
-//     </header>
-//   </div>
-//   );
-// }
 
 export default App;
