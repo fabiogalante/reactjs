0dc16216f3314e7ce1d2c770ca64e6bcba0ce23d
Fabio Galante Mans <fabiogalantemans@gmail.com>
2021-05-15 14:51:53 -0300

    Componentes de classe com estado (stateful)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Component} from "react";
 
 class App extends Component {
     state = {
+        counter: 0,
         posts: [
             {
                 id: 1,
@@ -21,10 +22,28 @@ class App extends Component {
         ]
     };
 
+    componentDidMount() {
+        this.handleTimeOut();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+
+    }
+
+    handleTimeOut = () => {
+        const {posts, counter} = this.state;
+        posts[0].title = 'O titulo foi mudado';
+        setTimeout(() => {
+            this.setState({posts, counter: counter + 1});
+        }, 2000)
+    }
+
+
     render() {
-        const {posts} = this.state;
+        const {posts, counter} = this.state;
         return (
             <div className="App">
+                <h1>{counter}</h1>
                 {posts.map(post => (
                     <div key={post.id}>
                         <h1>{post.title}</h1>
@@ -36,5 +55,27 @@ class App extends Component {
     }
 }
 
+
+// componentDidMount() {
+//     setTimeout(() => {
+//         this.setState({
+//             posts: [
+//                 {
+//                     id: 1,
+//                     title: 'O título 1',
+//                     body: 'O corpo 1'
+//                 }, {
+//                     id: 2,
+//                     title: 'O título 1',
+//                     body: 'O corpo 1'
+//                 }, {
+//                     id: 3,
+//                     title: 'O título 3',
+//                     body: 'O corpo 3'
+//                 },
+//             ]
+//         })
+//     }, 5000);
+// }
 
 export default App;
